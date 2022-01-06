0898a50d165fb82f45f75bbdc9f01b3abbbe6ab0
Paul Oms <support@ohmysmtp.com>
2022-01-06 12:29:55 +0000

    Adding twitter image

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, keywords, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `twitter:image`,
+          content: `https://blog.ohmysmtp.com/logo.png`,
+        },
       ]
         .concat(
           keywords.length > 0
